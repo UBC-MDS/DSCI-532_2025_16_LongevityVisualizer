d816d3a8e05e9d9b7e9585324e4cb0f05dec5c12
AbdulSafdar <moizsafd@student.ubc.ca>
2025-03-12 11:41:13 -0700

    This commit will load the data as a parquet file, and create a processed data file (#76)
    * This commit will load the data as a parquet file, and create a processed data file
    
    * This commit adds loading animations for the charts and cards
    
    * This commit adds caching functionality to loading the dataset

--- src/app.py
+++ src/app.py
@@ -8,6 +8,7 @@ import sys
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, project_root)
 
+from src.cache_config import cache 
 from src.data import load_data, get_unique_years, load_geodata
 from src.components import create_layout
 from src.callbacks import register_callbacks
@@ -19,7 +20,7 @@ alt.data_transformers.enable("vegafusion")
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.title = "Longevity Visualizer"
 server = app.server  # Define server at module level for Gunicorn to find
-
+cache.init_app(server) #Initialize the caching
 
 def main():
     # Load and preprocess data
--- src/data.py
+++ src/data.py
@@ -1,8 +1,21 @@
 import pandas as pd
 import geopandas as gpd
+from src.cache_config import cache  
 
+import os
+import pandas as pd
+
+
+@cache.memoize()
 def load_data():
-    """Load and preprocess the Gapminder dataset."""
+    """Load and preprocess the Gapminder dataset, saving it as a Parquet file."""
+    parquet_path = "data/processed/gapminder_data.parquet"
+    
+    # Load the parquet data
+    if os.path.exists(parquet_path):
+        return pd.read_parquet(parquet_path)
+
+    # Preprocessing raw data
     df = pd.read_csv("data/raw/gapminder_data_graphs.csv")
     df = df.dropna(
         subset=[
@@ -16,6 +29,11 @@ def load_data():
             "services",
         ]
     )
+    
+    # Save to Parquet for future use
+    os.makedirs("data/processed", exist_ok=True)  # Ensure the directory exists
+    df.to_parquet(parquet_path, engine="pyarrow", index=False)
+    
     return df
 
 def get_unique_years(df, step=4):
@@ -49,6 +67,7 @@ CONTINENT_COLORS = {
     "South America": "#8c564b",  # Brown
 }
 
+@cache.memoize()
 def load_geodata():
     geo_df = gpd.read_file("data/processed/gapminder.json")
 
