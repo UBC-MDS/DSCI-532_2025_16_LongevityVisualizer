b57f2c2732bc8edac27ec4a6e6f6f7800c6d4c68
AbdulSafdar <moizsafd@student.ubc.ca>
2025-03-13 20:11:32 -0700

    Update app.py to be able to deploy the server

--- src/app.py
+++ src/app.py
@@ -42,5 +42,5 @@ def main():
 app = main()
 
 if __name__ == "__main__":
-    app.server.run(debug=False)  # Use run_server instead of server.run
+    app.run_server(debug=False)  # Use run_server instead of server.run
 
